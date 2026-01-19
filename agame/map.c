73888983b1e2c249509210216c70b66e1ac9a0f5
Luca Bassi <luca96bass@gmail.com>
2026-01-19 10:07:20 +0100

    max height variable (very epic)

--- agame/map.c
+++ agame/map.c
@@ -29,32 +29,36 @@ void generate_column_for(uint8_t target_x_tile){
   // calc new world_blocks_y from current
   uint8_t new_world_blocks_y = 1; // default 1 as fallback in case something is wrong
   uint8_t r_height = (uint8_t)rand();
-  switch (world_blocks_y){
-    case 0:
-      new_world_blocks_y = r_height < 64 ? 0 : 1; // 1 in 4 arbitrarily
-      break;
-    case 1:
-      new_world_blocks_y = r_height < 85 ?  0      // 1/3
-        : (r_height < 170 ?                 1      // 1/3
-        :                                   2      // 1/3
-      );
-      break;
-    case 2:
-      new_world_blocks_y = r_height < 64 ? 1 : 2; // 1 in 4 arbitrarily
-      break;
+
+  if(world_blocks_y == 0){
+    // can only grow or stay the same
+    new_world_blocks_y = r_height < 64 ? 0 : 1; // 1 in 4 arbitrarily
+  }else if(world_blocks_y == WORLD_MAX_Y){
+    // can only shrink or stay the same
+    new_world_blocks_y = r_height < 64 ? 1 : 2; // 1 in 4 arbitrarily
+  }else{
+    // can safely do +-1 randomly
+    new_world_blocks_y = r_height < 85 ?  world_blocks_y-1  // 1/3
+      : (r_height < 170 ?                 world_blocks_y    // 1/3
+      :                                   world_blocks_y+1  // 1/3
+    );
   }
 
-  uint8_t platform_block_height  = new_world_blocks_y + 1;  // number of solid blocks in that col (or solid height of the col)
-  uint8_t col_has_cloud = 0;                                // at most one cloud per col
   const unsigned char *block_tile;                          // kind of tile
+  uint8_t platform_block_height  = new_world_blocks_y + 1;  // number of solid blocks in that col (or solid height of the col)
   // get a new block for every block of the col (from the bottom)
-  // we increase by 1 cause eventually we'll shift the whole col DOWN according to the STEP_HEIGHT (we have to guarantee to overwrite every "old" tile, so up to y=0)
-  // "1" in this case is just roof(WORLD_MAX_Y (2) * STEP_HEIGHT_TILES (2) / BLOCK_HEIGHT_TILES (4))
-  // but untill i find a clever way to make the max world level customizable (not 2) might as well just write 1
+  // we have to go for more than COL_HEIGHT (based on WORLD_MAX_Y)
+  // cause eventually we'll shift the whole col DOWN according to the STEP_HEIGHT and new_world_blocks_y (we have to guarantee to overwrite every "old" tile)
+  // this does mean that we potentially draw out of bounds for higher levels but its not necessarily relevant since you dont see it
+  // also its easy to add an if() to not draw them if it becomes an issue
   // is it the best way to do it? idk but its what i thought of
-  for(uint8_t block_i=0; block_i<COL_HEIGHT+1; block_i++){
+  uint8_t shift_offset = platform_block_height*STEP_HEIGHT_TILES;
+  // ceiling of ratio to know how many tiles needed to cover the post-shift hole
+  uint8_t height_to_cover = COL_HEIGHT + ( (shift_offset+BLOCK_HEIGHT_TILES-1)/BLOCK_HEIGHT_TILES );
+  uint8_t col_has_cloud = 0; // at most one cloud per col
+  for(uint8_t block_i=0; block_i<height_to_cover; block_i++){
     if(block_i < platform_block_height){
-      // between 0 and the solid height (1/2/3)
+      // between 0 and the solid height
       uint8_t r_block = (uint8_t)rand();
 
       if(block_i == platform_block_height - 1){
--- agame/variables.h
+++ agame/variables.h
@@ -1,5 +1,6 @@
 #define COL_HEIGHT          5 // 32x32 blocks in a col
 #define STEP_HEIGHT_TILES   2 // tile height of the step between two different (consecutive) height levels
+#define WORLD_MAX_Y         3 // max height of the solid blocks in a col
 
 #define BLOCK_WIDTH_PX     32
 #define BLOCK_WIDTH_TILES   4
